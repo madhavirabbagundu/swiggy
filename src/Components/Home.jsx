293e320584198d3683dfffc146b831d19146fa43
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-09-18 22:08:54 +0530

    Home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Home.css'
+
+const Home = ()=>{
+return(
+    <>
+    <h1>379 restaurent</h1>
+    <div id = "container">
+    <div class = "container1">
+     <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/fjbwlls9ol1tmurj8dhw'/>
+     <h4>Chinni's Biryani</h4>
+     <p>chinese</p>
+    </div>
+    <div class = "container1">
+    <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sogfy729opgp8ngsnraa'/>
+      <h4>Om Sai Ram Food Junction</h4>
+      <p>Biryani</p>
+    </div>
+    <div class = "container1">
+    <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/kux9pi1alwz6ptxeryjf'/>
+      <h4>Nati Grills</h4>
+      <p>Paan</p>
+    </div>
+    <div class = "container1">      
+     <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xig2adse3dotmouvksfu'/>
+    <h4>meals</h4>
+    <p>American</p>
+    </div>
+    <div class = "container1">     
+   <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qoztwkeh7e7crgsobs09'/>
+    <h4>paan ki Dukaan</h4>
+    <p>Chinese</p>
+    </div>
+    <div class = "container1">
+    <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xdus7zt6uimq0bbqzk8w'/>
+    <h4>Sri ramji Sweets</h4>
+    <p>North Indian</p>
+    </div>
+    <div class = "container1">
+    <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/dwc8omvzlcwnlgk8160z'/>
+   <h4>Kanti Sweets</h4>
+   <p>South Indian</p>
+    </div>
+    <div class = "container1">
+    <img src = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/nplevbrcqo7iobpvrny9'/>
+    <h4>bakasura Bandhi</h4>
+    <p>Fast Food</p>
+    </div>
+
+    </div>
+    </>
+)
+}
+export default Home
